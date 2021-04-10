e44d8da628d9446b0e228d285bf89f81d32ff7bb
WHITE71wolf <71555090+WHITE71wolf@users.noreply.github.com>
2021-04-10 03:16:10 +0300

    Update maker-list.py

--- maker-list.py
+++ maker-list.py
@@ -1,6 +1,13 @@
-import random 
+import random
 import string
+import os
 
+
+def maker_close():
+	if os.name == 'nt':
+		os.system('cls')
+	else:
+		os.system('clear')
 
 let = 0
 
@@ -11,27 +18,27 @@ Dark="\033[0;37;40m"
 
 
 
+maker_close()
 print("""
-
-  ▄▄▌  ▪  .▄▄ · ▄▄▄▄▄    • ▌ ▄ ·.  ▄▄▄· ▄ •▄ ▄▄▄ .▄▄▄  
-  ██•  ██ ▐█ ▀. •██      ·██ ▐███▪▐█ ▀█ █▌▄▌▪▀▄.▀·▀▄ █·
-  ██▪  ▐█·▄▀▀▀█▄ ▐█.▪    ▐█ ▌▐▌▐█·▄█▀▀█ ▐▀▀▄·▐▀▀▪▄▐▀▀▄ 
-  ▐█▌▐▌▐█▌▐█▄▪▐█ ▐█▌·    ██ ██▌▐█▌▐█ ▪▐▌▐█.█▌▐█▄▄▌▐█•█▌
-  . ▀▀▀ ▀▀▀ ▀▀▀▀  ▀▀▀     ▀▀  █▪▀▀▀ ▀  ▀ ·▀  ▀ ▀▀▀ .▀  ▀
- ═════════════════════════════════════════════════════
-
+ ██╗     ██╗███████╗████████╗              ███╗   ███╗ █████╗ ██╗  ██╗███████╗██████╗ 
+ ██║     ██║██╔════╝╚══██╔══╝              ████╗ ████║██╔══██╗██║ ██╔╝██╔════╝██╔══██╗
+ ██║     ██║███████╗   ██║       █████╗    ██╔████╔██║███████║█████╔╝ █████╗  ██████╔╝
+ ██║     ██║╚════██║   ██║       ╚════╝    ██║╚██╔╝██║██╔══██║██╔═██╗ ██╔══╝  ██╔══██╗
+ ███████╗██║███████║   ██║                 ██║ ╚═╝ ██║██║  ██║██║  ██╗███████╗██║  ██║
+ ╚══════╝╚═╝╚══════╝   ╚═╝                 ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═╝
+ ════════════════════════════════════════════════════════════════════════════════════
   The programmer is not responsible for any wrong event
-  TikTok: @l.7a'
-
+  TikTok: @l.7a
+  GitHub: WHITE71wolf
 """)
 
-text = string.ascii_lowercase + string.digits
-additions = input('additions: ')
-lengit = int(input('length: '))
-count = int(input('count: '))
+text = string.ascii_lowercase + string.digits 
+additions = input('[+] Additions: ')
+lengit = int(input('[+] Length: '))
+count = int(input('[+] Count: '))
 for x in range(count):
     pasb = ''.join(random.choice(text) for x in range(lengit))
-    with open('list.txt', 'a') as file:
+    with open('user.txt', 'a') as file:
         file.write(additions + pasb + '\n')
         let += 1
         print(f'Done {let}\{count}')
