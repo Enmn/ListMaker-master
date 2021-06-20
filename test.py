d2e1e1fbfe050b2c1876a297067400308a9355f8
WHITE71wolf <71555090+WHITE71wolf@users.noreply.github.com>
2021-06-21 00:21:26 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
 import random
 import string
 import os
-
+import urllib.request
+import urllib.parse
+import time
+import sys
 
 def maker_close():
 	if os.name == 'nt':
 		os.system('cls')
 	else:
 		os.system('clear')
-		r
 
 let = 0
 
@@ -79,8 +81,42 @@ if o =="3":
 if o =="4":
   os.system('exit')
 
-  
 
+
+
+
+
+
+  
+def update():
+    stuff_to_update = ['test.py', 'start.bat', '.version']
+    for fl in stuff_to_update:
+        dat = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/" + fl).read()
+        file = open(fl, 'wb')
+        file.write(dat)
+        file.close()
+    print('\n\t\tUpdated Successfull !!!!')
+    print('\tRun The Script Again...')
+    exit()
+
+try:
+    urllib.request.urlopen('https://www.google.com')
+except Exception:
+    print("Error While Connecting To Internet!!!")
+    print("\tPlease Connect To Internet To Continue...\n")
+    input('Exiting....\n Press Enter To Exit....')
+    exit()
+print('\tChecking For Updates...')
+ver = urllib.request.urlopen(".version").read().decode('utf-8')
+verl = ''
+try:
+    verl = open(".version", 'r').read()
+except Exception:
+    pass
+if ver != verl:
+    print('\n\t\tAn Update is Available....')
+    print('\tUpdating Anon-SMS...')
+    update()
 
 
         
