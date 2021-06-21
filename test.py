fc1e624ea395ec29ff6d3a4baad14ca05c3c967c
WHITE71wolf <71555090+WHITE71wolf@users.noreply.github.com>
2021-06-21 03:19:39 +0300

    Update test.py

--- test.py
+++ test.py
@@ -23,38 +23,6 @@ text = string.ascii_lowercase + string.digits
 number = string.digits
 email2 = string.ascii_lowercase
 
-def update():
-  stuff_to_update = ['test.py', '.version', 'start.bat' 'update.py']
-  for fl in stuff_to_update:
-    dat = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/" + fl).read()
-    file = open(fl, 'wb')
-    file.write(dat)
-    file.close()
-    print('\n\t\tUpdated Successfull !!!!')
-    print('\tRun The Script Again...')
-    exit()
-
-try:
-    urllib.request.urlopen('https://www.google.com')
-except Exception:
-    print("Error While Connecting To Internet!!!")
-    print("\tPlease Connect To Internet To Continue...\n")
-    input('Exiting....\n Press Enter To Exit....')
-    exit()
-ver = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/.version").read().decode('utf-8')
-verl = ''
-try:
-    verl = open(".version", 'r').read()
-except Exception:
-    pass
-if ver != verl:
-    print('\n\t\tAn Update is Available....')
-    print('\tUpdating...')
-   
-
-maker_close()
-
-update()
 
 maker_close()
 print("""
@@ -116,6 +84,34 @@ if o =="4":
 
 
 
+def update():
+  stuff_to_update = ['test.py', '.version', 'start.bat' 'update.py']
+  for fl in stuff_to_update:
+    dat = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/" + fl).read()
+    file = open(fl, 'wb')
+    file.write(dat)
+    file.close()
+    print('\n\t\tUpdated Successfull !!!!')
+    print('\tRun The Script Again...')
+    exit()
+
+try:
+    urllib.request.urlopen('https://www.google.com')
+except Exception:
+    print("Error While Connecting To Internet!!!")
+    print("\tPlease Connect To Internet To Continue...\n")
+    input('Exiting....\n Press Enter To Exit....')
+    exit()
+ver = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/.version").read().decode('utf-8')
+verl = ''
+try:
+    verl = open(".version", 'r').read()
+except Exception:
+    pass
+if ver != verl:
+    print('\n\t\tAn Update is Available....')
+    print('\tUpdating...')
+    update()
 
 
 
