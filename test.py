c5e7813bf7e84364162182ab083f22454d696b2a
WHITE71wolf <71555090+WHITE71wolf@users.noreply.github.com>
2021-06-21 03:26:28 +0300

    Update test.py

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ def maker_close():
 		os.system('cls')
 	else:
 		os.system('clear')
-		rr
+		
 		
 
 let = 0
@@ -85,34 +85,7 @@ if o =="4":
 
 
 
-def update():
-  stuff_to_update = ['test.py', '.version', 'start.bat']
-  for fl in stuff_to_update:
-    dat = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/" + fl).read()
-    file = open(fl, 'wb')
-    file.write(dat)
-    file.close()
-    print('\n\t\tUpdated Successfull !!!!')
-    print('\tRun The Script Again...')
-    exit()
 
-try:
-    urllib.request.urlopen('https://www.google.com')
-except Exception:
-    print("Error While Connecting To Internet!!!")
-    print("\tPlease Connect To Internet To Continue...\n")
-    input('Exiting....\n Press Enter To Exit....')
-    exit()
-ver = urllib.request.urlopen("https://raw.githubusercontent.com/WHITE71wolf/ListMaker-master/master/.version").read().decode('utf-8')
-verl = ''
-try:
-    verl = open(".version", 'r').read()
-except Exception:
-    pass
-if ver != verl:
-    print('\n\t\tAn Update is Available....')
-    print('\tUpdating...')
-    update()
 
 
 
